565db882d1098c62f125b70ed34cfb05b847c18e
XploR5 <brohan5501@gmail.com>
2023-02-07 17:56:07 +0530

    base skilliton

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {DragDropModule} from '@angular/cdk/drag-drop';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeadComponent } from './head/head.component';
+import { ShortAnswerComponent } from './short-answer/short-answer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeadComponent,
+    ShortAnswerComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    DragDropModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/short-answer/short-answer.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-short-answer',
+  templateUrl: './short-answer.component.html',
+  styleUrls: ['./short-answer.component.scss']
+})
+export class ShortAnswerComponent {
+
+}
