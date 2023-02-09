7681a39aa726fbeb61f7b27198cbd8c3d8820e9b
XploR5 <brohan5501@gmail.com>
2023-02-09 14:36:12 +0530

    To Do -add components

--- src/app/create-form/create-form.component.ts
+++ src/app/create-form/create-form.component.ts
@@ -1,26 +1,29 @@
 import {Component} from '@angular/core';
-import {CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag-drop';
+import {CdkDragDrop, copyArrayItem, moveItemInArray, transferArrayItem} from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'cdk-drag-drop-connected-sorting-group-example',
   templateUrl: 'create-form.component.html',
   styleUrls: ['create-form.component.scss'],
 })
-export class CreateFormComponent {
-  todo = ['Get to work', 'Pick up groceries', 'Go home', 'Fall asleep'];
 
-  done = ['Get up', 'Brush teeth', 'Take a shower', 'Check e-mail', 'Walk dog'];
+export class CreateFormComponent {
+  todo = ['Title', 'short ans', 'single correct', 'multiple correct','Date', 'email', 'number'];
+
+  done = ['Title',];
 
   drop(event: CdkDragDrop<string[]>) {
-    if (event.previousContainer === event.container) {
-      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
-    } else {
-      transferArrayItem(
-        event.previousContainer.data,
-        event.container.data,
-        event.previousIndex,
-        event.currentIndex,
-      );
-    }
+  if (event.previousContainer === event.container) {
+    moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+  } else if (event.previousContainer.id === 'todo' && event.container.id === 'done') {
+    copyArrayItem(
+      event.previousContainer.data,
+      event.container.data,
+      event.previousIndex,
+      event.currentIndex,
+    );
+  } else if (event.previousContainer.id === 'done' && event.container.id === 'todo') {
+    event.previousContainer.data.splice(event.previousIndex, 1);
   }
+}
 }
